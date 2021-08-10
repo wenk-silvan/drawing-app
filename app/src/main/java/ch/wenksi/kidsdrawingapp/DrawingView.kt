09c8b791e2990202504b4628344d57a73c872d02
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2021-08-10 14:46:28 +0200

    Initial commit - hello world app

---
+++ app/src/main/java/ch/wenksi/kidsdrawingapp/DrawingView.kt
@@ -0,0 +1,9 @@
+package ch.wenksi.kidsdrawingapp
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+}
